185321da3a90eb42cb09dbbd2da23a1eb32d0b0e
abhitiw3147 <104640389+abhitiw3147@users.noreply.github.com>
2024-08-08 10:39:42 +0000

    Basic Topic Covered

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,26 @@
+/* --- Keep Points --- */
+// alert(1+1) // this is not allowed in nodejs,allowed in browser we have alternative for alert in js
+// 1) Maintain Code Reability always
+
+// 2) Data Type in Js are :-
+
+/*
+
+Number - Numeric Value like int,float,etc but imp it range in memory is 2 => pow 53
+bigInt - Range is bigger than Number useful in trading,stock app
+String - anything that is in single Quote(') or Double Quote ("")
+Boolean - true/false this boolean values
+null - it is standalone value and datatype also it is useful in some special case like temperature app 
+undefind - when we declare variable but not assign any value to that variable
+symbol - uniqueness 
+
+object - advance datatype in js
+
+*/
+
+/* --- typeof- it is function which will tell us variable is what kind of datatype  --- */
+let num = 154;
+console.log(typeof "Abhishek");
+console.log(typeof null) // interview question may ask on this what is the data type of null.null is object.
+console.log(typeof undefined) // interview question what is the data type of undefined.undefined is undefined.
+console.log(typeof num)
---
+++ 01_basic/variables.js
@@ -0,0 +1,19 @@
+const id = 11123 // const keyword is used for value that cannot be changed through the program
+
+let cityName = 'Mumbai'; // let keyword is used for to have some reserve space in memory
+
+var stateName = 'Maharashtra'; // var keyword is used for to have some reserve space in memory & have scope concept
+
+countryName = 'India'; // WE can declare variable like this also
+
+let xyz;
+
+console.log(id) // console.log() is function which will print give parameter in () on terminal
+
+//to print table in structure use
+
+console.table ([id,cityName,stateName,countryName,xyz])
+
+/* --- Note --- */
+
+// When we declare variable without value like variable xyz it give result undefined.
